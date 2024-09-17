c0432f8aee901775cc8622112576160735cce4e6
SchoolNigga <>
2024-09-17 10:33:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ class Geom:
     name="Geom"
 
 class Line(Geom):
-    def draf(self):
+    def draw(self):
         print("Linijas zimesana")
-
-g=Genom()
+    def set_cords(self, x1, y1, x2, y2):
+        self.x1=x1
+        self.y1=y1
+        self.x2=x2
+        self.y2=y2
+        print(x1, y1, x2, y2)
+g=Geom()
 l=Line()
 l.draw()
+l.set_cords(1,4,5,7)
 print(l.name)
