804cff78e6fec52d662b2a84678e430ba5fd608c
SchoolNigga <>
2024-09-17 10:47:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ class Geom:
         self.y2=y2
         print(x1, y1, x2, y2)
 class Line(Geom):
+    name="Line"
     def draw(self):
         print("Linijas zimesana")
 
 class Rect(Geom):
     def draw(self):
         print("Rect zimesana")   
-#g=Geom()
+g=Geom()
 l=Line()
 r=Rect()
 l.set_cords(1,4,5,7)
