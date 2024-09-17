07f49a7790f3a5ae0b8d7bce6068a0c90941f9e7
SchoolNigga <>
2024-09-17 11:17:15 +0300

    Update auto.py

--- auto.py
+++ auto.py
@@ -7,5 +7,14 @@ class Vehicle:
         def display_info(self):
               print(f"Make: {self.make},Model: {self.model},Year: {self.year},Price: {self.price}")
 
+class Car(Vehicle):
+        def __init__(self, make, model, year, price, num_doors, body_style):
+            super().__init__(make, model, year, price) 
+            self.num_doors = num_doors
+            self.body_style = body_style
+        def display_info(self):
+            print(f"Make: {self.make},Model: {self.model},Year: {self.year},Price: {self.price},body_style: {self.body_style},num_doors: {self.num_doors}")   
 a=Vehicle("ko?","top","1488","shawel",)
 a.display_info()
+c=Car("ko?","top","1488","shawel","4","nice")
+c.display_info()
